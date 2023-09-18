28ed2876077d9a264c32bf5f63aec9dbfebeda58
Alejandro <ale11199@estudiantec.cr>
2023-09-17 21:19:33 -0600

    remove time sleep functions

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,24 @@
-import java.sql.Time;
 import java.util.Scanner;
 
 /**
- * Esta clase funcionara como ejecutable para probar la implementacion del juego de la vida.
+ * Esta clase funciona como ejecutable para probar la implementación del juego de la vida.
  */
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese el tamaño del tablero en el universo: ");
         int n = scanner.nextInt();
-        System.out.print("Ingrese el tiempo que desea esperar entre generaciones (segundos): ");
-        double t = scanner.nextDouble();
 
         Universo genActual = new Universo(n);
         int i = 0;
         genActual.colocarVivas(n);
-        System.out.println("Generacion inicial:");
+        System.out.println("Generación inicial:");
         System.out.println(genActual);
         while (i < 1000) {
             if (genActual.getCantVivas() == 0) break;
             Universo sigGen = genActual.aplicaReglas();
-            Thread.sleep((long) t * 1000);
-            System.out.println("Evolucion " + i + ":");
+            //Thread.sleep((long) t * 1000);
+            System.out.println("Evolución " + (i + 1) + ":");
             System.out.println(sigGen);
             genActual = new Universo(sigGen);
             i++;
